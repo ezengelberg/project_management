b3308d49674c2b45d84b6e34df046d1a74480ac0
Maxwell <maxims970@gmail.com>
2024-10-13 20:36:06 +0300

    Basic User & Project Schemas

---
+++ server/models/Project.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const ProjectSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    advisor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+});
