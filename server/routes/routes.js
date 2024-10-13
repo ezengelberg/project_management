56cc3211ebd0f5c18cd36661ffb0695611fb2d54
Maxwell <maxims970@gmail.com>
2024-10-13 20:36:52 +0300

    routes and packaging

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 
+const {loginUser} = require("../controllers/UserController");
+
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send(`Server API is running on port ${5000}...`);
 });
+
+router.post('/login', loginUser);
 
 module.exports = router;
