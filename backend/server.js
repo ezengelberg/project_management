640c9562880ba219a9e71078eb7c8347ddaadc4e
Maxwell <maxims970@gmail.com>
2024-10-15 20:48:39 +0300

    added a package to be able to use req body

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 import { connectDB } from "./config/db.js";
+
+import User from "./models/users.js";
+import userRoute from "./routes/userRoute.js";
 
 // Load environment variables and allows to use .env file
 dotenv.config();
 
 const app = express();
 const server_port = process.env.SERVER_PORT || 3000;
+
+
+// 
+app.use(cors());
+
+// Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+app.use(bodyParser.json());
+
+app.use("/api/user", userRoute);
 
 app.listen(server_port, () => {
   connectDB();
