dce3704640b40c5c2f00369c13ceccc5affe0a20
Maxwell <maxims970@gmail.com>
2024-10-15 18:36:09 +0300

    added more fields

--- backend/models/projects.js
+++ backend/models/projects.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
 const projectSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    year: { type: Number, required: true },
-    status: { type: String, required: true, default: "open" }, // status of the project, is it taken or not yet
-    suitableFor: { type: String, required: true }, // solo / duo / both
-    advisor: [{ type: String, required: false, default: []}],
-    students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, default: [] }],
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  year: { type: Number, required: true }, // year the student is doing the project לאיזה שנתון שייך הפרוייקט
+  isAvilable: { type: Boolean, required: true, default: false }, // is it taken or not yet
+  suitableFor: { type: String, required: true }, // solo / duo / both
+  type: { type: String, required: true }, // research / development / hitech / other
+  status: { type: String, required: true, default: "new" }, // New / Continues
+  isApproved : { type: Boolean, required: true, default: false }, // is it approved by the coordinator
+  advisor: [{ type: mongoose.Schema.Types.ObjectId, ref: "User", required: false, default: [] }],
+  students: [{ type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, default: [] }]
 });
 
 const projectModel = mongoose.model("Project", projectSchema);
--- backend/models/users.js
+++ backend/models/users.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    id : { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isStudent: { type: Number, required: true, default: true }, // year the student is doing the project
     isAdvisor: { type: Boolean, required: true, default: false },
