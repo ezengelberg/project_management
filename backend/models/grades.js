62faae30993e352cc0ede82cb9475e8909bc2cfe
Maxwell <maxims970@gmail.com>
2024-10-15 19:09:44 +0300

    updated grades schema

--- backend/models/grades.js
+++ backend/models/grades.js
@@ -4,9 +4,17 @@ const gradeSchema = new mongoose.Schema(
   {
     grade: { type: Number, required: true },
     type: { type: String, required: true }, // alpha / beta / presentation / final
-    project: { type: mongoose.Schema.Types.ObjectId, ref: "Project", required: true },
-    judge: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    comment: { type: String, required: false, default: "" }
+    project: { type: mongoose.Schema.Types.ObjectId, ref: "Project", required: true }, // the project the grade is for
+    judge: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // the judge that gave the grade
+    comment: { type: String, required: false, default: "" }, // the comment the judge gave
+    overridden: {
+      type: {
+        by: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false, default: null }, // the coordinator that overriden the grade
+        comment: { type: String, required: false, default: "" }, // the comment the coordinator
+        newGrade: { type: Number, required: false, default: null } // the new grade the coordinator
+      },
+      required: false
+    } // if the grade was overriden by the coordinator
   },
   { timestamps: true }
 );
