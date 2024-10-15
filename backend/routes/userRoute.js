f63b55f921401fb9f907d1b3c7fa80da694cbd8b
Maxwell <maxims970@gmail.com>
2024-10-15 20:48:49 +0300

    added basic user login / register

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,59 @@
+import express from "express";
+import bcrypt from "bcrypt";
+
+import User from "../models/users.js";
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, id, password, confirmPassword, isStudent, isAdvisor, isCoordinator } = req.body;
+    const user = await User.findOne({
+      email: email
+    });
+    if (user) {
+      return res.status(400).send("User already exists");
+    }
+    if (password !== confirmPassword) {
+      return res.status(400).send("Passwords do not match");
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      name,
+      email,
+      id,
+      password: hashedPassword,
+      isStudent: true, // set default for now
+      isAdvisor: false, // set default for now
+      isCoordinator: false // set default for now
+    });
+    await newUser.save();
+    console.log(`User ${name} registered successfully`);
+    res.status(201).send("User registered successfully");
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log(`Email: ${email}`);
+    console.log(`Password: ${password}`);
+    const user = await User.findOne({
+      email: email
+    });
+    if (!user) {
+      return res.status(401).send("User or password do not match");
+    }
+    if (await bcrypt.compare(password, user.password)) {
+      res.send("Login successful");
+    } else {
+      return res.status(401).send("User or password do not match");
+    }
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+export default router;
