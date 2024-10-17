2e48ec7bd1191a01e188a7b2bf0f57f15daf2aeb
Maxwell <maxims970@gmail.com>
2024-10-17 23:06:38 +0300

    added wrong path design

--- frontend/src/components/WrongPath/WrongPath.jsx
+++ frontend/src/components/WrongPath/WrongPath.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import "./WrongPath.css";
 
 const WrongPath = () => {
-return (
+  return (
     <div className="wrong-path">
-        <div className="not-found">
-            <h1>עמוד זה עדיין אינו קיים</h1>
-            <img src={require('./404.jpeg')} alt="404 Not Found" />
-            <h2>נסו לשוב במועד מאוחר יותר</h2>
-        </div>
+      <div className="not-found">
+        <h1>אופס...</h1>
+        <h2>העמוד שניסית להגיע אליו לא קיים</h2>
+        <img src={require("./404.jpeg")} alt="404 Not Found" />
+      </div>
     </div>
-);
+  );
 };
 
 export default WrongPath;
