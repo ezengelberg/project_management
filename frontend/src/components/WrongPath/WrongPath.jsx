bca798372485b1e3870fee89703050c637a74854
Maxwell <maxims970@gmail.com>
2024-10-18 00:10:50 +0300

    page not found v2

--- frontend/src/components/WrongPath/WrongPath.jsx
+++ frontend/src/components/WrongPath/WrongPath.jsx
@@ -6,8 +6,9 @@ const WrongPath = () => {
     <div className="wrong-path">
       <div className="not-found">
         <h1>אופס...</h1>
-        <h2>העמוד שניסית להגיע אליו לא קיים</h2>
-        <img src={require("./404.jpeg")} alt="404 Not Found" />
+        <h2>העמוד שניסית להגיע אליו כבר איננו פה</h2>
+        <img src={require("./image.png")} alt="404 Not Found" />
+        <button className="return-to-existance">חזור למקום קיים</button>
       </div>
     </div>
   );
