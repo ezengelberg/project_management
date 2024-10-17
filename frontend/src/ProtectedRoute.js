8adce2ec58e9d8583b308546f9b0bdb9776c465d
Maxwell <maxims970@gmail.com>
2024-10-17 16:15:39 +0300

    removed unneeded for now

--- frontend/src/ProtectedRoute.js
+++ frontend/src/ProtectedRoute.js
@@ -1,9 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = !!localStorage.getItem("user"); // Check if user is stored (can also use a state or context)
-
   return isAuthenticated ? children : <Navigate to="/login" />; // Redirect to login if not authenticated
 };
 
