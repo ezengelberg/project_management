7d0e2dd7b0e743df16f6bda9fd85deab681208aa
Maxwell <maxims970@gmail.com>
2024-11-02 20:20:29 +0200

    added some control panel buttons

--- frontend/src/components/SystemControl/SystemControl.jsx
+++ frontend/src/components/SystemControl/SystemControl.jsx
@@ -1,37 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SystemControl.scss";
+import { Button, Switch } from "antd";
+import { Tooltip } from "antd";
+import { CloseCircleOutlined } from "@ant-design/icons";
 
 const SystemControl = () => {
+  const [createProject, setCreateProject] = useState(true);
+  const [registerToProjects, setRegisterToProjects] = useState(true);
+  const [manageStudents, setManageStudents] = useState(true);
   return (
     <div className="system">
-      <div className="switches">
-        <div className="switch">
-          <label className="switch-label">הפעל מערכת</label>
-          <label className="switch">
-            <input type="checkbox" />
-            <span className="slider round"></span>
-          </label>
+      <h1 className="system-title">לוח בקרת מערכת</h1>
+      <div className="control-options">
+        <div className="box switches">
+          <h3 className="box-title">סוויטצ'ים להדלקה \ כיבוי מהירים</h3>
+          <div className="switch">
+            <label className="switch-label">הזנת פרוייקטים חדשים</label>
+            <Switch checked={createProject} />
+          </div>
+          <Tooltip title="רישום של הסטודנטים עצמם לפרוייקט">
+            <div className="switch">
+              <label className="switch-label">רישום לפרוייקטים</label>
+              <Switch checked={registerToProjects} />
+            </div>
+          </Tooltip>
+          <Tooltip title="אישור, דחיה והסרה של סטודנטים מפרויקט">
+            <div className="switch">
+              <label className="switch-label">ניהול סטודנטים בפרוייקט</label>
+              <Switch checked={manageStudents} />
+            </div>
+          </Tooltip>
+          <div className="switch">
+            <label className="switch-label">pending action</label>
+            <Switch />
+          </div>
         </div>
-        <div className="switch">
-          <label className="switch-label">הפעל פרויקטים</label>
-          <label className="switch">
-            <input type="checkbox" />
-            <span className="slider round"></span>
-          </label>
-        </div>
-        <div className="switch">
-          <label className="switch-label">הפעל סטודנטים</label>
-          <label className="switch">
-            <input type="checkbox" />
-            <span className="slider round"></span>
-          </label>
-        </div>
-        <div className="switch">
-          <label className="switch-label">הפעל מנהלים</label>
-          <label className="switch">
-            <input type="checkbox" />
-            <span className="slider round"></span>
-          </label>
+        <div className="box finish-projects">
+          <h3 className="box-title">סיים פרוייקטים</h3>
+          <Tooltip title="סיים את כל הפרוייקטים">
+            <Button shape="circle" type="primary" icon={<CloseCircleOutlined />}></Button>
+          </Tooltip>
         </div>
       </div>
     </div>
