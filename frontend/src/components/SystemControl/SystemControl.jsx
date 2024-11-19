d030e66518aea6686100038677c185c67d808fe2
Adam Shay <adam19255@gmail.com>
2024-11-19 19:26:26 +0200

    started grade  table

--- frontend/src/components/SystemControl/SystemControl.jsx
+++ frontend/src/components/SystemControl/SystemControl.jsx
@@ -1,13 +1,123 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./SystemControl.scss";
-import { Button, Switch } from "antd";
-import { Tooltip } from "antd";
-import { CloseCircleOutlined } from "@ant-design/icons";
+import { Button, Switch, Form, Input, InputNumber, Table, Typography, message, Tooltip } from "antd";
+import { CloseCircleOutlined, EditOutlined, SaveOutlined, StopOutlined } from "@ant-design/icons";
 
 const SystemControl = () => {
   const [createProject, setCreateProject] = useState(true);
   const [registerToProjects, setRegisterToProjects] = useState(true);
   const [manageStudents, setManageStudents] = useState(true);
+  const [form] = Form.useForm();
+  const [data, setData] = useState([]);
+  const [editingKey, setEditingKey] = useState("");
+  const [loading, setLoading] = useState(false);
+  const isEditing = (record) => record.key === editingKey;
+
+  const EditableCell = ({ editing, dataIndex, title, inputType, record, index, children, ...restProps }) => {
+    const inputNode = inputType === "number" ? <InputNumber /> : <Input />;
+    return (
+      <td {...restProps}>
+        {editing ? (
+          <Form.Item
+            name={dataIndex}
+            style={{
+              margin: 0,
+            }}
+            rules={[
+              {
+                required: true,
+                message: "שדה חובה",
+              },
+            ]}>
+            {inputNode}
+          </Form.Item>
+        ) : (
+          children
+        )}
+      </td>
+    );
+  };
+
+  const edit = (record) => {
+    form.setFieldsValue({ ...record });
+    setEditingKey(record.key);
+  };
+
+  const cancel = () => {
+    setEditingKey("");
+    form.resetFields();
+  };
+
+  const save = async (key) => {
+    try {
+      const row = await form.validateFields();
+      const newData = [...data];
+      const index = newData.findIndex((item) => key === item.key);
+      if (index > -1) {
+        const item = newData[index];
+        const updatedItem = { ...item, ...row };
+        newData.splice(index, 1, updatedItem);
+        setData(newData);
+        setEditingKey("");
+        form.resetFields();
+      }
+    } catch (errInfo) {
+      console.log("Validate Failed:", errInfo);
+    }
+  };
+
+  const columns = [
+    {
+      title: "פעולות",
+      dataIndex: "actions",
+      key: "actions",
+      render: (_, record) => {
+        const editable = isEditing(record);
+        return editable ? (
+          <span>
+            <Typography.Link
+              onClick={() => save(record.key)}
+              style={{
+                marginInlineEnd: 8,
+              }}>
+              <Tooltip title="שמירה">
+                <SaveOutlined className="edit-icon" />
+              </Tooltip>
+            </Typography.Link>
+            <a onClick={cancel}>
+              <Tooltip title="ביטול">
+                <StopOutlined className="edit-icon" />
+              </Tooltip>
+            </a>
+          </span>
+        ) : (
+          <Typography.Link disabled={editingKey !== ""} onClick={() => edit(record)}>
+            <Tooltip title="עריכה">
+              <EditOutlined className="edit-icon" />
+            </Tooltip>
+          </Typography.Link>
+        );
+      },
+      width: "10%",
+    },
+  ];
+
+  const mergedColumns = columns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        inputType: "text",
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: isEditing(record),
+      }),
+    };
+  });
+
   return (
     <div className="system">
       <h1 className="system-title">לוח בקרת מערכת</h1>
@@ -42,6 +152,23 @@ const SystemControl = () => {
           </Tooltip>
         </div>
       </div>
+      <Form form={form} component={false} loading={loading}>
+        <Table
+          style={{ width: "100%" }}
+          components={{
+            body: {
+              cell: EditableCell,
+            },
+          }}
+          bordered
+          dataSource={data}
+          columns={mergedColumns}
+          rowClassName="editable-row"
+          pagination={{
+            onChange: cancel,
+          }}
+        />
+      </Form>
     </div>
   );
 };
