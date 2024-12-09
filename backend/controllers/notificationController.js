aa85df3d54abf4b3f40c156390e7553b1ed9f578
Adam Shay <adam19255@gmail.com>
2024-12-09 11:33:49 +0200

    Added notification page

--- backend/controllers/notificationController.js
+++ backend/controllers/notificationController.js
@@ -1,8 +1,17 @@
 import Notification from "../models/notifications.js";
 
-export const getNotifications = async (req, res) => {
+export const getUnreadNotifications = async (req, res) => {
   try {
     const notifications = await Notification.find({ user: req.user._id, read: false });
+    res.status(200).json(notifications);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const getAllNotifications = async (req, res) => {
+  try {
+    const notifications = await Notification.find({ user: req.user._id });
     res.status(200).json(notifications);
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
@@ -23,6 +32,16 @@ export const markNotificationAsRead = async (req, res) => {
     const { notificationId } = req.params;
     await Notification.findByIdAndUpdate(notificationId, { read: true });
     res.status(200).json({ message: "Notification marked as read" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const deleteNotification = async (req, res) => {
+  try {
+    const { notificationId } = req.params;
+    await Notification.findByIdAndDelete(notificationId);
+    res.status(200).json({ message: "Notification deleted" });
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
   }
