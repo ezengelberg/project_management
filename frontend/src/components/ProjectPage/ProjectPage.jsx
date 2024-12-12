15bae8bfdbf46586c10bb4dd0d7f9c60588ed54d
Maxwell <maxims970@gmail.com>
2024-12-12 20:20:15 +0200

    removed tabs from project page

--- frontend/src/components/ProjectPage/ProjectPage.jsx
+++ frontend/src/components/ProjectPage/ProjectPage.jsx
@@ -1,19 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { InfoCircleOutlined, BookOutlined, UserOutlined, LoadingOutlined } from "@ant-design/icons";
 import { Tooltip, message, Spin } from "antd";
+import { UserOutlined, LoadingOutlined } from "@ant-design/icons";
 import "./ProjectPage.scss";
 import { processContent } from "../../utils/htmlProcessor";
 
 const ProjectPage = () => {
   const { projectID } = useParams();
   const [projectData, setProjectData] = useState({});
-  const [page, setPage] = useState({
-    Information: true,
-    Grades: false,
-    Other: false
-  });
   const [advisors, setAdvisors] = useState([]);
   const [isCandidate, setIsCandidate] = useState(false);
   const [hasProject, setHasProject] = useState(false);
@@ -29,8 +24,8 @@ const ProjectPage = () => {
         const response = await axios.get(
           `${process.env.REACT_APP_BACKEND_URL}/api/project/check-if-candidate/${projectID}`,
           {
-            withCredentials: true
-          }
+            withCredentials: true,
+          },
         );
         setIsCandidate(response.data.isCandidate);
       } catch (error) {
@@ -44,7 +39,7 @@ const ProjectPage = () => {
           const advisors = [];
           for (const advisor of projectData.advisors) {
             const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/user/get-user-info/${advisor}`, {
-              withCredentials: true
+              withCredentials: true,
             });
             advisors.push(response.data);
           }
@@ -65,8 +60,8 @@ const ProjectPage = () => {
         const response = await axios.get(
           `${process.env.REACT_APP_BACKEND_URL}/api/user/check-user-has-projects/${user._id}`,
           {
-            withCredentials: true
-          }
+            withCredentials: true,
+          },
         );
         setHasProject(response.data.hasProject);
       } catch (error) {
@@ -76,19 +71,11 @@ const ProjectPage = () => {
     hasProject();
   }, []);
 
-  const switchPage = (pageName) => {
-    const myPages = { ...page };
-    for (const key in page) {
-      myPages[key] = key === pageName;
-    }
-    setPage(myPages);
-  };
-
   useEffect(() => {
     const fetchProjectData = async () => {
       try {
         const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/project/get-project/${projectID}`, {
-          withCredentials: true
+          withCredentials: true,
         });
         setProjectData(response.data);
       } catch (error) {
@@ -109,23 +96,23 @@ const ProjectPage = () => {
       setIsLoading(true);
       message.open({
         type: "loading",
-        content: "מבצע הסרת הרשמה מהפרויקט..."
+        content: "מבצע הסרת הרשמה מהפרויקט...",
       });
       const response = await axios.get(`/api/user/get-user`, {
-        withCredentials: true
+        withCredentials: true,
       });
       const userID = response.data._id;
       await axios.post(
         `${process.env.REACT_APP_BACKEND_URL}/api/project/remove-candidate`,
         { projectID: projectID, userID: userID },
-        { withCredentials: true }
+        { withCredentials: true },
       );
       console.log("Unsignup successful");
       setTimeout(() => {
         message.open({
           type: "success",
           content: "הוסר הרשמה מהפרויקט",
-          duration: 2
+          duration: 2,
         });
       }, 500);
     } catch (error) {
@@ -143,19 +130,19 @@ const ProjectPage = () => {
       setIsLoading(true);
       message.open({
         type: "loading",
-        content: "מבצע הרשמה לפרויקט..."
+        content: "מבצע הרשמה לפרויקט...",
       });
       await axios.post(
         `${process.env.REACT_APP_BACKEND_URL}/api/project/add-candidate`,
         { projectID: projectID },
-        { withCredentials: true }
+        { withCredentials: true },
       );
       console.log("Signup successful");
       setTimeout(() => {
         message.open({
           type: "success",
           content: "נרשם בהצלחה",
-          duration: 2
+          duration: 2,
         });
       }, 500);
     } catch (error) {
@@ -164,7 +151,7 @@ const ProjectPage = () => {
         message.open({
           type: "error",
           content: error.response.data.message,
-          duration: 2
+          duration: 2,
         });
       }, 500);
     } finally {
@@ -188,90 +175,68 @@ const ProjectPage = () => {
           </div>
         ))}
       </div>
-      <div className="project-profile-navbar">
-        <div
-          className={`project-navbar-item ${page.Information ? "navbar-item-selected" : ""}`}
-          onClick={() => switchPage("Information")}>
-          <InfoCircleOutlined />
-          <div className="project-navbar-item-name">פרטי הפרויקט</div>
-        </div>
-        <div
-          className={`project-navbar-item ${page.Grades ? "navbar-item-selected" : ""}`}
-          onClick={() => switchPage("Grades")}>
-          <BookOutlined />
-          <div className="project-navbar-item-name">ציונים</div>
-        </div>
-        <div
-          className={`project-navbar-item ${page.Other ? "navbar-item-selected" : ""}`}
-          onClick={() => switchPage("Other")}>
-          <div className="project-navbar-item-icon"></div>
-          <div className="project-navbar-item-name">לא יודע עוד משהו</div>
-        </div>
-      </div>
       <hr className="project-profile-line"></hr>
-      {page.Information && (
-        <div className="project-profile-info project-profile-content">
-          <div className="project-profile-info-item">
-            <div className="project-profile-info-title">תיאור הפרויקט</div>
-            <div
-              className="project-profile-info-text rich-text-content"
-              dangerouslySetInnerHTML={{ __html: processContent(projectData.description) }}></div>
-          </div>
-          <div className="project-profile-info-item project-badges">
-            <div className="project-profile-info-title">התאמות:</div>
-            <Tooltip title="מתאים ל">
-              <div className="project-badge project-suitable">{projectData.suitableFor}</div>
-            </Tooltip>
-            <Tooltip title="סוג פרויקט">
-              <div className="project-badge project-type">{projectData.type}</div>
-            </Tooltip>
-          </div>
-          <div className="project-profile-info-item project-profile-info-emails">
-            <div className="project-profile-info-title">אימייל המרצה ליצירת קשר:</div>
-            <div className="project-profile-info-text project-profile-info-email">
-              {advisors.map((advisor, index) => (
-                <Tooltip key={index} title="לחץ להעתקה">
-                  <div
-                    className="advisor-email"
-                    onClick={() => {
-                      copyToClipboard();
-                    }}>
-                    {advisor.email}
-                  </div>
-                </Tooltip>
-              ))}
-              {advisors.length === 0 && <div>אין מנחה לפרויקט זה כרגע</div>}
-            </div>
-          </div>
-          {!hasProject && user.isStudent ? (
-            isCandidate ? (
-              <div className="project-unsignup-button" onClick={() => Unsignup()}>
-                {isLoading ? (
-                  <div className="loading-registration">
-                    <Spin indicator={<LoadingOutlined spin />} />
-                    <span>מבצע הסרת הרשמה</span>
-                  </div>
-                ) : (
-                  "הסר הרשמה מפרויקט"
-                )}
-              </div>
-            ) : (
-              <div className="project-signup-button" onClick={() => Signup()}>
-                {isLoading ? (
-                  <div className="loading-registration">
-                    <Spin indicator={<LoadingOutlined spin />} />
-                    <span>מבצע הרשמה</span>
-                  </div>
-                ) : (
-                  "הרשמה לפרויקט"
-                )}
-              </div>
-            )
-          ) : (
-            ""
-          )}
+      <div className="project-profile-info project-profile-content">
+        <div className="project-profile-info-item">
+          <div className="project-profile-info-title">תיאור הפרויקט</div>
+          <div
+            className="project-profile-info-text rich-text-content"
+            dangerouslySetInnerHTML={{ __html: processContent(projectData.description) }}></div>
         </div>
-      )}
+        <div className="project-profile-info-item project-badges">
+          <div className="project-profile-info-title">התאמות:</div>
+          <Tooltip title="מתאים ל">
+            <div className="project-badge project-suitable">{projectData.suitableFor}</div>
+          </Tooltip>
+          <Tooltip title="סוג פרויקט">
+            <div className="project-badge project-type">{projectData.type}</div>
+          </Tooltip>
+        </div>
+        <div className="project-profile-info-item project-profile-info-emails">
+          <div className="project-profile-info-title">אימייל המרצה ליצירת קשר:</div>
+          <div className="project-profile-info-text project-profile-info-email">
+            {advisors.map((advisor, index) => (
+              <Tooltip key={index} title="לחץ להעתקה">
+                <div
+                  className="advisor-email"
+                  onClick={() => {
+                    copyToClipboard();
+                  }}>
+                  {advisor.email}
+                </div>
+              </Tooltip>
+            ))}
+            {advisors.length === 0 && <div>אין מנחה לפרויקט זה כרגע</div>}
+          </div>
+        </div>
+        {!hasProject && user.isStudent ? (
+          isCandidate ? (
+            <div className="project-unsignup-button" onClick={() => Unsignup()}>
+              {isLoading ? (
+                <div className="loading-registration">
+                  <Spin indicator={<LoadingOutlined spin />} />
+                  <span>מבצע הסרת הרשמה</span>
+                </div>
+              ) : (
+                "הסר הרשמה מפרויקט"
+              )}
+            </div>
+          ) : (
+            <div className="project-signup-button" onClick={() => Signup()}>
+              {isLoading ? (
+                <div className="loading-registration">
+                  <Spin indicator={<LoadingOutlined spin />} />
+                  <span>מבצע הרשמה</span>
+                </div>
+              ) : (
+                "הרשמה לפרויקט"
+              )}
+            </div>
+          )
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 };
