59d3d1cce56be671de909fd8f5a6c387d3f23560
Maxwell <maxims970@gmail.com>
2024-12-28 14:20:35 +0200

    added option to set system's current hebrew year

--- backend/models/config.js
+++ backend/models/config.js
@@ -1,7 +1,16 @@
 import mongoose from "mongoose";
+import { HDate } from "@hebcal/core";
+
+// Function to calculate and format the current Jewish year
+const getDefaultJewishYear = () => {
+  const today = new HDate().renderGematriya(true);
+  const jewishYear = today.split(" ")[2];
+  return jewishYear;
+};
 
 const configSchema = new mongoose.Schema({
   projectStudentManage: { type: Boolean, default: true },
+  currentYear: { type: String, default: getDefaultJewishYear },
 });
 
 const configModel = mongoose.model("Config", configSchema);
