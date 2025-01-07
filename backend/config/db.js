a8d5158ac88f6259fd062bfb0f099cbedebf0691
Maxwell <maxims970@gmail.com>
2025-01-07 19:53:26 +0200

    updating db connection differences

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 export const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1); // Exit with failure
-    }
+  try {
+    const conn = await mongoose.connect(
+      process.env.NODE_ENV === "production" ? process.env.MONGO_URI : process.env.MONGO_URI_LOCAL,
+    );
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1); // Exit with failure
+  }
 };
