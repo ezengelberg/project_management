b5bd3a57bd31bd18f0bb696a1f3f09472e511500
Maxwell <maxims970@gmail.com>
2025-01-07 16:53:01 +0200

    cookie

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,6 @@ app.use(
       maxAge: 24 * 60 * 60 * 1000, // Cookie will expire after 1 day
       secure: process.env.NODE_ENV === "production", // Set to true if using HTTPS
       sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // Use Lax for local development
-      httpOnly: true, // This prevents JavaScript access
       domain: process.env.NODE_ENV === "production" ? new URL(process.env.CORS_ORIGIN).hostname : undefined,
     },
   }),
