5899205cbfe98a32d3a9c63651d08ad6f860b6b9
Maxwell <maxims970@gmail.com>
2025-01-09 09:32:58 +0200

    changed 404 page design

--- frontend/src/components/WrongPath/WrongPath.jsx
+++ frontend/src/components/WrongPath/WrongPath.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import "./WrongPath.scss";
 import { useNavigate } from "react-router-dom";
+import { Result, Button } from "antd";
 
 const WrongPath = () => {
   const navigate = useNavigate();
   return (
-    <div className="wrong-path">
-      <div className="not-found">
-        <h1>אופס...</h1>
-        <h2>העמוד שניסית להגיע אליו כבר איננו פה</h2>
-        <img src={require("../../assets/404v2.jpg")} alt="404 Not Found" />
-        <button className="return-to-existance" onClick={() => navigate("/home")}>
-          חזור למקום קיים
-        </button>
-      </div>
-    </div>
-  );
+    <div className="wrong-path"><Result
+    status="404"
+    title="404"
+    subTitle="סליחה, העמוד שהגעת אליו אינו קיים"
+    extra={<Button type="primary" onClick={()=> navigate("/login")}>למסך כניסה</Button>}
+  /></div>
+);
 };
 
 export default WrongPath;
