7e1ff281c62baf5dfd91df6a8e8341f06cdce672
Maxwell <maxims970@gmail.com>
2025-02-02 17:30:32 +0200

    Added search bar in new chat

--- frontend/src/components/Chat/Chat.jsx
+++ frontend/src/components/Chat/Chat.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import "./Chat.scss";
 
-import { CloseOutlined, UserOutlined } from "@ant-design/icons";
+import { CloseOutlined, UserOutlined, SearchOutlined } from "@ant-design/icons";
+import { Input } from "antd";
 
 const Chat = ({ type }) => {
     const [participants, setParticipants] = useState([
@@ -11,6 +12,10 @@ const Chat = ({ type }) => {
         "גיא נתן איתי",
         "גיא נתן איתי",
     ]);
+    const fetchChat = async () => {
+        setParticipants(["אביאבי 12", "גיא נתן איתי", "גיא נתן איתי", "גיא נתן איתי", "גיא נתן איתי"]);
+    };
+
     return (
         <div className="chat-container">
             <CloseOutlined className="chat-close" />
@@ -28,6 +33,11 @@ const Chat = ({ type }) => {
                                 <div className="participant-name">{participant}</div>
                             </div>
                         ))}
+                        {participants.length === 0 && <div className="no-participants">לא הוספו משתמשים</div>}
+                    </div>
+                    <div className="chat-search-container">
+                        <Input className="chat-search" placeholder="חפש משתמש" onChange={() => console.log("search users")}/>
+                        <SearchOutlined className="search-icon"/>
                     </div>
                 </div>
             ) : (
