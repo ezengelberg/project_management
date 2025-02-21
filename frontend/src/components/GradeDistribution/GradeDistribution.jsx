56ea926791867f01c7654fd5b457a49f9ba89da3
Maxwell <maxims970@gmail.com>
2025-02-21 23:50:33 +0200

    added warnings before fetch

--- frontend/src/components/GradeDistribution/GradeDistribution.jsx
+++ frontend/src/components/GradeDistribution/GradeDistribution.jsx
@@ -1,225 +1,219 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Tabs, Select, Table, Input, Radio } from "antd";
+import { Tabs, Select, Table, Input, Radio, message } from "antd";
 import { toJewishDate, formatJewishDateInHebrew } from "jewish-date";
 import "./GradeDistribution.scss";
 
 const GradeDistribution = () => {
-  const [years, setYears] = useState([]);
-  const [selectedYear, setSelectedYear] = useState("");
-  const [advisors, setAdvisors] = useState([]);
-  const [submissionOptions, setSubmissionOptions] = useState([]);
-  const [letters, setLetters] = useState({
-    "A+": 0,
-    A: 0,
-    "A-": 0,
-    "B+": 0,
-    B: 0,
-    "B-": 0,
-    "C+": 0,
-    C: 0,
-    "C-": 0,
-    "D+": 0,
-    D: 0,
-    "D-": 0,
-    E: 0,
-    F: 0,
-  });
+    const [years, setYears] = useState([]);
+    const [selectedYear, setSelectedYear] = useState("");
+    const [advisors, setAdvisors] = useState([]);
+    const [submissionOptions, setSubmissionOptions] = useState([]);
+    const [letters, setLetters] = useState({
+        "A+": 0,
+        A: 0,
+        "A-": 0,
+        "B+": 0,
+        B: 0,
+        "B-": 0,
+        "C+": 0,
+        C: 0,
+        "C-": 0,
+        "D+": 0,
+        D: 0,
+        "D-": 0,
+        E: 0,
+        F: 0,
+    });
 
-  const fetchYear = async () => {
-    const response = await axios.get(
-      `${process.env.REACT_APP_BACKEND_URL}/api/config/get/year`,
-      {
-        withCredentials: true,
-      }
-    );
-    setYears(response.data.years);
-  };
+    const fetchYear = async () => {
+        const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/config/get/year`, {
+            withCredentials: true,
+        });
+        setYears(response.data.years);
+    };
 
-  const fetchAdvisors = async () => {
-    if (!selectedYear || selectedYear === "") return;
-    const response = await axios.get(
-      `${process.env.REACT_APP_BACKEND_URL}/api/user/get-active-advisors/`,
-      {
-        params: { year: selectedYear },
-        withCredentials: true,
-      }
-    );
-    setAdvisors(response.data);
-    console.log(response.data);
-  };
+    const fetchAdvisors = async () => {
+        if (!selectedYear || selectedYear === "") return;
+        const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/user/get-active-advisors/`, {
+            params: { year: selectedYear },
+            withCredentials: true,
+        });
+        setAdvisors(response.data);
+        console.log(response.data);
+    };
 
-  const fetchSubmissions = async () => {
-    if (!selectedYear || selectedYear === "") return;
-    const response = await axios.get(
-      `${process.env.REACT_APP_BACKEND_URL}/api/submission/get-yearly-submissions`,
-      {
-        params: { year: selectedYear },
-        withCredentials: true,
-      }
-    );
-    setSubmissionOptions(response.data);
-  };
+    const fetchSubmissions = async () => {
+        if (!selectedYear || selectedYear === "") return;
+        const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/submission/get-yearly-submissions`, {
+            params: { year: selectedYear },
+            withCredentials: true,
+        });
+        setSubmissionOptions(response.data);
+    };
 
-  const pickSubmissionDistribution = async (value) => {
-    if (!submissionOptions.includes(value)) return; // invalid submission option
-    if (selectedYear === "") return;
-  };
+    const pickSubmissionDistribution = async (value) => {
+        if (!submissionOptions.includes(value)) return; // invalid submission option
+        if (selectedYear === "") return;
+    };
 
-  const pickAdvisorDistribution = async (value) => {
-    if (!advisors.map((advisor) => advisor._id).includes(value)) return; // invalid advisor option
-    console.log(value);
-  };
+    const pickAdvisorDistribution = async (value) => {
+        if (!advisors.map((advisor) => advisor._id).includes(value)) return; // invalid advisor option
+        if (selectedYear === "") return;
+    };
 
-  useEffect(() => {
-    fetchYear();
-  }, []);
+    useEffect(() => {
+        fetchYear();
+    }, []);
 
-  useEffect(() => {
-    fetchAdvisors();
-    fetchSubmissions();
-  }, [selectedYear]);
+    useEffect(() => {
+        fetchAdvisors();
+        fetchSubmissions();
+    }, [selectedYear]);
 
-  const handleInputChange = (grade, newValue) => {
-    if (newValue < 0 || newValue > 100) return; // Invalid input
-    setLetters((prevLetters) => ({
-      ...prevLetters,
-      [grade]: newValue, // Update the specific grade with new value
-    }));
-  };
+    const handleInputChange = (grade, newValue) => {
+        if (newValue < 0 || newValue > 100) return; // Invalid input
+        setLetters((prevLetters) => ({
+            ...prevLetters,
+            [grade]: newValue, // Update the specific grade with new value
+        }));
+    };
 
-  const columns = [
-    {
-      title: "ציון",
-      dataIndex: "grade",
-      key: "grade",
-    },
-    {
-      title: "ערך",
-      dataIndex: "value",
-      key: "value",
-      render: (text, record) => (
-        <Input
-          type="number"
-          value={record.value}
-          onChange={(e) => {
-            handleInputChange(record.grade, e.target.value);
-          }}
-        />
-      ),
-    },
-  ];
-
-  const letterTableRender = () => {
-    return (
-      <Table
-        dataSource={Object.entries(letters).map(([grade, value]) => ({
-          key: grade,
-          grade,
-          value,
-        }))}
-        columns={columns}
-        pagination={false}
-      />
-    );
-  };
-
-  const items = [
-    {
-      key: "1",
-      label: "התפלגות ציונים לפי הגשה",
-      children: (
-        <>
-          <div className="select-options">
-            <Select
-              defaultValue="pick_year"
-              options={[
-                { value: "pick_year", label: "בחירת שנה" },
-                ...years.map((year) => ({
-                  value: year,
-                  label: year,
-                })),
-              ]}
-              onChange={(value) => setSelectedYear(value)}
-            />
-            <Select
-              defaultValue="pick_submission"
-              options={[
-                { value: "pick_submission", label: "בחר הגשה" },
-                ...submissionOptions.map((submission) => ({
-                  value: submission,
-                  label: submission,
-                })),
-              ]}
-              onChange={(value) => pickSubmissionDistribution(value)}
-            />
-          </div>
-          <div className="tab-content">
-            <div className="graph-zone">graph</div>
-            <div className="value-table">
-              <div className="grading-sum">
-                <Radio.Group
-                  defaultValue={"average"}
-                  options={[
-                    {
-                      value: "average",
-                      label: "ממוצע",
-                    },
-                    {
-                      value: "median",
-                      label: "חציון",
-                    },
-                  ]}
+    const columns = [
+        {
+            title: "ציון",
+            dataIndex: "grade",
+            key: "grade",
+        },
+        {
+            title: "ערך",
+            dataIndex: "value",
+            key: "value",
+            render: (text, record) => (
+                <Input
+                    type="number"
+                    value={record.value}
+                    onChange={(e) => {
+                        handleInputChange(record.grade, e.target.value);
+                    }}
                 />
-              </div>
-              {letterTableRender()}
-            </div>
-          </div>
-        </>
-      ),
-    },
-    {
-      key: "2",
-      label: "התפלגות ציונים לפי שופט",
-      children: (
-        <>
-          <div className="select-options">
-            <Select
-              defaultValue="pick_year"
-              options={[
-                { value: "pick_year", label: "בחירת שנה" },
-                ...years.map((year) => ({
-                  value: year,
-                  label: year,
-                })),
-              ]}
-              onChange={(value) => setSelectedYear(value)}
+            ),
+        },
+    ];
+
+    const letterTableRender = () => {
+        return (
+            <Table
+                dataSource={Object.entries(letters).map(([grade, value]) => ({
+                    key: grade,
+                    grade,
+                    value,
+                }))}
+                columns={columns}
+                pagination={false}
             />
-            <Select
-              defaultValue="pick_judge"
-              options={[
-                { value: "pick_judge", label: "בחר שופט" },
-                ...advisors.map((advisor) => ({
-                  value: advisor._id,
-                  label: advisor.name,
-                })),
-              ]}
-              onChange={(value) => pickAdvisorDistribution(value)}
-            />
-          </div>
-          <div className="tab-content">
-            <div className="graph-zone">graph</div>
-            <div className="value-table">{letterTableRender()}</div>
-          </div>
-        </>
-      ),
-    },
-  ];
-  return (
-    <div>
-      <Tabs defaultActiveKey="1" items={items} />
-    </div>
-  );
+        );
+    };
+
+    const items = [
+        {
+            key: "1",
+            label: "התפלגות ציונים לפי הגשה",
+            children: (
+                <>
+                    <div className="select-options">
+                        <Select
+                            defaultValue="pick_year"
+                            options={[
+                                { value: "pick_year", label: "בחירת שנה" },
+                                ...years.map((year) => ({
+                                    value: year,
+                                    label: year,
+                                })),
+                            ]}
+                            onChange={(value) => setSelectedYear(value)}
+                        />
+                        <Select
+                            defaultValue="pick_submission"
+                            options={[
+                                { value: "pick_submission", label: "בחר הגשה" },
+                                ...submissionOptions.map((submission) => ({
+                                    value: submission,
+                                    label: submission,
+                                })),
+                            ]}
+                            onChange={(value) => pickSubmissionDistribution(value)}
+                        />
+                    </div>
+                    <div className="tab-content">
+                        <div className="graph-zone">graph</div>
+                        <div className="grading-column">
+                            <div className="calc-type">
+                                <Radio.Group
+                                    defaultValue={"average"}
+                                    options={[
+                                        {
+                                            value: "average",
+                                            label: "ממוצע",
+                                        },
+                                        {
+                                            value: "median",
+                                            label: "חציון",
+                                        },
+                                    ]}
+                                />
+                            </div>
+                            {letterTableRender()}
+                        </div>
+                    </div>
+                </>
+            ),
+        },
+        {
+            key: "2",
+            label: "התפלגות ציונים לפי שופט",
+            children: (
+                <>
+                    <div className="select-options">
+                        <Select
+                            defaultValue="pick_year"
+                            options={[
+                                { value: "pick_year", label: "בחירת שנה" },
+                                ...years.map((year) => ({
+                                    value: year,
+                                    label: year,
+                                })),
+                            ]}
+                            onChange={(value) => setSelectedYear(value)}
+                        />
+                        <Select
+                            defaultValue="pick_judge"
+                            options={[
+                                { value: "pick_judge", label: "בחר שופט" },
+                                ...advisors.map((advisor) => ({
+                                    value: advisor._id,
+                                    label: advisor.name,
+                                })),
+                            ]}
+                            onChange={(value) => {
+                                if (selectedYear === "" || !selectedYear) return message.error("נא לבחור שנה");
+                                pickAdvisorDistribution(value);
+                            }}
+                        />
+                    </div>
+                    <div className="tab-content">
+                        <div className="graph-zone">graph</div>
+                        <div className="grading-column">{letterTableRender()}</div>
+                    </div>
+                </>
+            ),
+        },
+    ];
+    return (
+        <div>
+            <Tabs defaultActiveKey="1" items={items} />
+        </div>
+    );
 };
 
 export default GradeDistribution;
