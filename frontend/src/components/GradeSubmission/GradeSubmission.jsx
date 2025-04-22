e97aa81d8df46ce11a59cddc04f2db661cf9a018
Adam Shay <145905613+Adam19255@users.noreply.github.com>
2025-04-22 15:25:10 +0300

    added grades information table

--- frontend/src/components/GradeSubmission/GradeSubmission.jsx
+++ frontend/src/components/GradeSubmission/GradeSubmission.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState, useContext } from "react";
 import "./GradeSubmission.scss";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { Button, Form, Input, Select, message, Spin } from "antd";
+import { Button, Form, Input, Select, message, Spin, Tooltip, Modal, Descriptions } from "antd";
+import { InfoCircleTwoTone } from "@ant-design/icons";
 import { NotificationsContext } from "../../utils/NotificationsContext";
 import WrongPath from "../WrongPath/WrongPath";
 
@@ -19,6 +20,7 @@ const GradeSubmission = () => {
   const [project, setProject] = useState({});
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [isGradeInfoOpen, setIsGradeInfoOpen] = useState(false);
   const letterGrades = ["A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-", "E", "F"];
 
   useEffect(() => {
@@ -183,7 +185,14 @@ const GradeSubmission = () => {
             )}
             {project?.isGraded && (
               <Form.Item
-                label="ציון לפרויקט"
+                label={
+                  <div className="grade-input-label">
+                    <span>ציון לפרויקט</span>
+                    <Tooltip title="משמעות הציונים">
+                      <InfoCircleTwoTone className="info-icon" onClick={() => setIsGradeInfoOpen(true)} />
+                    </Tooltip>
+                  </div>
+                }
                 name="grade"
                 rules={[
                   {
@@ -219,6 +228,51 @@ const GradeSubmission = () => {
             </Form.Item>
           </Form>
         </div>
+      )}
+
+      {isGradeInfoOpen && (
+        <Modal
+          title="משמעות הציונים"
+          open={isGradeInfoOpen}
+          onCancel={() => setIsGradeInfoOpen(false)}
+          okButtonProps={{ style: { display: "none" } }}
+          cancelText="סגור"
+          width={800}>
+          <Descriptions bordered>
+            <Descriptions.Item label="+A" span={{ md: 3 }}>
+              <strong>פרויקט מבריק באופן יוצא דופן.</strong> לא כדאי לתת את הציון הזה ליותר מפרויקט אחד שאתם בודקים.
+              אפשר לא לתת את הציון הזה לשום פרויקט שאתם בודקים.
+            </Descriptions.Item>
+            <Descriptions.Item label="A" span={{ md: 3 }}>
+              <strong>פרויקט טוב מן הרגיל.</strong> כלומר: רואים שהתלמידים השקיעו והצליחו יותר ממה שהיינו מצפים מהם,
+              ויותר מרוב התלמידים האחרים.
+            </Descriptions.Item>
+            <Descriptions.Item label="-A" span={{ md: 3 }}></Descriptions.Item>
+            <Descriptions.Item label="+B" span={{ md: 3 }}></Descriptions.Item>
+            <Descriptions.Item label="B" span={{ md: 3 }}>
+              <strong>פרויקט רגיל.</strong> כלומר: התלמידים עשו עבודה יפה, כמו שצריך. ניתן לצפות שזה יהיה הציון החציוני
+              שלכם. הרבה תלמידים צריכים לקבל את הציון הזה. יהיו תלמידים שיקבלו יותר, ויהיו שיקבלו פחות.
+            </Descriptions.Item>
+            <Descriptions.Item label="-B" span={{ md: 3 }}></Descriptions.Item>
+            <Descriptions.Item label="+C" span={{ md: 3 }}></Descriptions.Item>
+            <Descriptions.Item label="C" span={{ md: 3 }}>
+              <strong>פרויקט פחות טוב מן הרגיל.</strong> כלומר: עבודת התלמידים סבירה, אבל יש חוסרים.
+            </Descriptions.Item>
+            <Descriptions.Item label="-C" span={{ md: 3 }}></Descriptions.Item>
+            <Descriptions.Item label="+D" span={{ md: 3 }}></Descriptions.Item>
+            <Descriptions.Item label="D" span={{ md: 3 }}>
+              <strong>פרויקט שחסרים בו הרבה דברים.</strong> כלומר: מגיע לתלמידים לעבור את הקורס, אבל העבודה לא טובה.
+            </Descriptions.Item>
+            <Descriptions.Item label="-D" span={{ md: 3 }}></Descriptions.Item>
+            <Descriptions.Item label="E" span={{ md: 3 }}>
+              <strong>פרויקט מאוד לא טוב.</strong> כלומר: מגיע לתלמידים להיכשל, אבל איננו רוצים לפגוש אותם בשנה הבאה.
+            </Descriptions.Item>
+            <Descriptions.Item label="F" span={{ md: 3 }}>
+              <strong>פרויקט שנכשל.</strong> תלמידים שיקבלו ציון כזה יחזרו, כנראה, על פרויקט הגמר שלהם בשנה הבאה. אם הם
+              יקבלו ציון כזה כמה שנים זו אחר זו ברצף, הם לא יקבלו תואר.
+            </Descriptions.Item>
+          </Descriptions>
+        </Modal>
       )}
     </div>
   );
