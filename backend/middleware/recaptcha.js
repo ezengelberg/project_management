67f745d00b79b5dabab9029d2605f25cecb711d3
Maxwell <maxims970@gmail.com>
2025-06-19 14:42:55 +0300

    Implement reCAPTCHA verification and add Docker Compose configuration for backend and testing services

--- backend/middleware/recaptcha.js
+++ backend/middleware/recaptcha.js
@@ -1,37 +1,40 @@
 import axios from "axios";
 
 const verifyRecaptcha = async (req, res, next) => {
-  const recaptchaToken = req.body.recaptchaToken;
-
-  // For testing purposes, uncomment the line below to skip reCAPTCHA verification and comment out the rest of the code
-  // next();
-
-  if (!recaptchaToken) {
-    return res.status(400).json({ message: "reCAPTCHA token is missing" });
-  }
-
-  if (!process.env.RECAPTCHA_SECRET_KEY) {
-    console.error("RECAPTCHA_SECRET_KEY is not set in the environment variables");
-    return res.status(500).json({ message: "Server configuration error" });
-  }
-
-  try {
-    const response = await axios.post(
-      `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${recaptchaToken}`
-    );
-
-    const { success, "error-codes": errorCodes } = response.data;
-
-    if (!success) {
-      console.error("reCAPTCHA verification failed:", errorCodes);
-      return res.status(400).json({ message: "reCAPTCHA verification failed", errorCodes });
+    // Skip verification if stress testing is active
+    if (process.env.STRESS_TEST_ACTIVE === "true") {
+        console.log("Skipping reCAPTCHA verification due to STRESS_TEST_ACTIVE");
+        return next();
     }
 
-    next();
-  } catch (error) {
-    console.error("reCAPTCHA verification error:", error.message);
-    return res.status(500).json({ message: "reCAPTCHA verification error. Please try again later." });
-  }
+    const recaptchaToken = req.body.recaptchaToken;
+
+    if (!recaptchaToken) {
+        return res.status(400).json({ message: "reCAPTCHA token is missing" });
+    }
+
+    if (!process.env.RECAPTCHA_SECRET_KEY) {
+        console.error("RECAPTCHA_SECRET_KEY is not set in the environment variables");
+        return res.status(500).json({ message: "Server configuration error" });
+    }
+
+    try {
+        const response = await axios.post(
+            `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${recaptchaToken}`,
+        );
+
+        const { success, "error-codes": errorCodes } = response.data;
+
+        if (!success) {
+            console.error("reCAPTCHA verification failed:", errorCodes);
+            return res.status(400).json({ message: "reCAPTCHA verification failed", errorCodes });
+        }
+
+        next();
+    } catch (error) {
+        console.error("reCAPTCHA verification error:", error.message);
+        return res.status(500).json({ message: "reCAPTCHA verification error. Please try again later." });
+    }
 };
 
 export default verifyRecaptcha;
